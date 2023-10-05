e119a044b4e3d7285dd9b7472951d44c3fa8d31c
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-10-06 07:15:22 +0800

    jdosajdios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,19 +25,23 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="mt-4 flex items-center justify-between">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Not registered?') }}
+                </a>
 
                 <x-button class="ml-4">
                     {{ __('Log in') }}
--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,13 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+    style="
+    background-image: url(https://th.bing.com/th/id/R.3540727256b68c8093caef1c09c94da8?rik=wH9v3%2f1D5DPIWg&riu=http%3a%2f%2ffarm5.static.flickr.com%2f4002%2f4253227494_8c37a5d627_b.jpg&ehk=AVsu0f0uXcBN5PmUeycJ9zmGpaWacWHgZtUBfBz5m6U%3d&risl=&pid=ImgRaw&r=0);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+">
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="flex items-center justify-center py-4 mb-3">{{ $logo }}</div>
         {{ $slot }}
     </div>
 </div>
