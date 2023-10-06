57c816cc969feeb1c96556007edd8bbff2f2c90d
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-10-06 14:22:43 +0800

    isdhklfsdhflksdjfhsdlkj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -831,6 +831,116 @@
                 padding: 2rem
             }
         }
+
+
+        .mySlides {
+            display: none;
+        }
+
+        .mySlides img {
+            vertical-align: middle;
+            filter: brightness(50%)
+        }
+
+        /* Slideshow container */
+        .slideshow-container {
+            max-width: 100%;
+            position: relative;
+            margin: auto;
+        }
+
+        /* Next & previous buttons */
+        .prev,
+        .next {
+            cursor: pointer;
+            position: absolute;
+            top: 50%;
+            width: auto;
+            padding: 16px;
+            margin-top: -22px;
+            color: white;
+            font-weight: bold;
+            font-size: 18px;
+            transition: 0.6s ease;
+            border-radius: 0 3px 3px 0;
+            user-select: none;
+        }
+
+        /* Position the "next button" to the right */
+        .next {
+            right: 0;
+            border-radius: 3px 0 0 3px;
+        }
+
+        /* On hover, add a black background color with a little bit see-through */
+        .prev:hover,
+        .next:hover {
+            background-color: rgba(0, 0, 0, 0.8);
+        }
+
+        /* Caption text */
+        .text {
+
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+            color: white;
+        }
+
+        /* Number text (1/3 etc) */
+        .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+            cursor: pointer;
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+        }
+
+        .active,
+        .dot:hover {
+            background-color: #717171;
+        }
+
+        /* Fading animation */
+        .fade {
+            animation-name: fade;
+            animation-duration: 1.5s;
+        }
+
+        @keyframes fade {
+            from {
+                opacity: .4
+            }
+
+            to {
+                opacity: 1
+            }
+        }
+
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+
+            .prev,
+            .next,
+            .text {
+                font-size: 11px
+            }
+        }
     </style>
 </head>
 
@@ -999,10 +1109,81 @@
         </div>
         <br> <br>
     </div>
+    <div>
+
+        <div class="slideshow-container">
+
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="img/check-up.jpg" style="width:100%" class="w-full">
+                <div class="text">
+                    <h1 class="text-5xl font-bold text-block">PET CHECK-UP</h1>
+                    <h2 class="text-3xl text-block">Assess your pet's general health.</h2>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="img/vaccine.jpg" style="width:100%" class="w-full">
+                <div class="text">
+                    <h1 class="text-5xl font-bold text-block">PET VACCINATION</h1>
+                    <h2 class="text-3xl text-block">Protect your pets against devastating and life-threatening
+                        diseases.</h2>
+                </div>
+            </div>
+
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="img/grooming.jpg" style="width:100%" class="w-full">
+                <div class="text">
+                    <h1 class="text-5xl font-bold text-block">PET GROOMING</h1>
+                    <h2 class="text-3xl text-block">Ensure your pet's comfort.</h2>
+                </div>
+            </div>
+
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
+
+        </div>
+
+        <div style="text-align:center" class="bg-slate-900 p-5">
+            <span class="dot" onclick="currentSlide(1)"></span>&nbsp;
+            <span class="dot" onclick="currentSlide(2)"></span>&nbsp;
+            <span class="dot" onclick="currentSlide(3)"></span>
+        </div>
+
+    </div>
 
 
+    <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
 
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
 
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        setInterval(() => {
+            currentSlide(slideIndex % 3 + 1);
+        }, 5000);
+
+        function showSlides(n) {
+            const slides = document.querySelectorAll(".mySlides");
+            const dots = document.querySelectorAll(".dot");
+
+            slideIndex = (n > slides.length) ? 1 : (n < 1) ? slides.length : n;
+
+            slides.forEach(slide => slide.style.display = "none");
+            dots.forEach(dot => dot.classList.remove("active"));
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].classList.add("active");
+        }
+    </script>
 </body>
 
 </html>
